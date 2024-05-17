497401fd08569c8c0488a9293ceadb6cd8708dfe
Rasyid1609 <rasyid.mahardian@gmail.com>
2024-05-17 22:35:58 +0700

    Nomads Travel Web App

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ url('frontend/libraries/query/code.jquery.com_jquery-3.7.0.min.js') }}"></script>
+<script src="{{ url('frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
+<script src="{{ url('frontend/libraries/retina/retina.min.js') }}"></script>
